6513f2d70860e0517379efab87c1abce6c45237d
Josh <Joshua.mugglestone@gmail.com>
2022-01-02 16:23:31 +0000

    tidy top menu components

--- app/components/topMenu/topMenuBar.js
+++ app/components/topMenu/topMenuBar.js
@@ -1,20 +1,29 @@
+/**
+ * @fileoverview this file represents a TopMenuBar compoenent used to render a top bar containg page name and a setting or back icon
+ */
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Feather } from '@expo/vector-icons';
 import TouchableMenuIcon from './touchableMenuIcon';
 
-
+/**
+ * TopMenuBar component, renders a top bar containg page name and a setting or back icon
+ * @param {object} navigation - Navigaction object to allow for moving between pages
+ * @param {string} title - title of the page
+ * @param {boolean} settingsButton - wether to display the settings icon
+ * @param {boolean} backButton - wether to display the back icon
+ * @param {Function} backOnPress - function to run when back button pressed
+ */
 const TopMenuBar = ({ navigation, title, settingsButton, backButton, backOnPress }) => {
     let SettingsOnPress = () => {
-        navigation.push("SettingsScreen")
+        navigation.push('SettingsScreen')
     }
     return (
         <View style={styles.container}>
-            <TouchableMenuIcon icon={"chevron-left"} active={backButton} onPress={backOnPress}/>
+            <TouchableMenuIcon icon={'chevron-left'} active={backButton} onPress={backOnPress}/>
             <Text style={styles.text}>
                 {title}
             </Text>
-            <TouchableMenuIcon icon={"settings"} active={settingsButton} onPress={SettingsOnPress}/>
+            <TouchableMenuIcon icon={'settings'} active={settingsButton} onPress={SettingsOnPress}/>
         </View>
     );
 };
@@ -22,17 +31,17 @@ const TopMenuBar = ({ navigation, title, settingsButton, backButton, backOnPress
 const styles = StyleSheet.create({
     container:{
         height: 50,
-        width: "100%",
-        flexDirection:"row",
+        width: '100%',
+        flexDirection:'row',
         justifyContent:'space-between',
-        backgroundColor: "#694fad",
-        alignItems: "center",
+        backgroundColor: '#694fad',
+        alignItems: 'center',
         paddingHorizontal: 10,
         paddingBottom: 10,
     },
     text:{
         fontSize: 30,
-        color: "white"
+        color: 'white'
     }
 });
 export default TopMenuBar;
